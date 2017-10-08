a86035d3f6a671e98635d70471f3fc66fe06ddc9
Jared Hanson <jaredhanson@gmail.com>
2017-10-07 21:56:02 -0700

    Add sitemap and robots.txt.

--- site.js
+++ site.js
@@ -9,6 +9,9 @@ site.use('/packages', require('kerouac-npm-packages')());
 
 site.content('content');
 
+site.page('/sitemap.xml', require('kerouac-sitemap')());
+site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
+site.page('/robots.txt', require('kerouac-robotstxt')());
 site.page('/CNAME', require('kerouac-cname')());
 
 
