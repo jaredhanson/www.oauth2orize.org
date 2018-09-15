0349c136513003f26477cfa310b00da053f77d50
Jared Hanson <jaredhanson@gmail.com>
2018-09-14 18:57:34 -0700

    Update site.

--- app/site.js
+++ app/site.js
@@ -3,24 +3,19 @@ exports = module.exports = function(packages, sitemap, robots, cname) {
   
   
   var site = kerouac();
+  site.engine('pug', require('pug'));
   
   site.set('base url', 'http://www.oauth2orize.org');
+  site.set('layout engine', 'pug');
+  site.locals.pretty = true;
 
-  site.engine('ejs', require('ejs'));
-
-  //site.use('/packages', require('kerouac-npm-packages')());
   site.use('/packages', packages);
 
   site.content('content');
 
-  //site.page('/sitemap.xml', require('kerouac-sitemap')());
-  //site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
-  //site.page('/robots.txt', require('kerouac-robotstxt')());
   site.use(sitemap);
   site.use(robots);
   site.use(cname);
-  
-  //site.page('/CNAME', require('kerouac-cname')());
   
   return site;
 };
