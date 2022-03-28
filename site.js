d3e6ed2fc0e6f34e8d63715c62cec8b77cb15bd2
Jared Hanson <jaredhanson@gmail.com>
2022-03-28 07:24:49 -0700

    Update dependencies and site generator.

--- site.js
+++ site.js
@@ -1,1 +1,24 @@
-require('bixby')('org.kerouacjs/main', { dev: true });
+//require('bixby')('org.kerouacjs/main', { dev: true });
+
+var kerouac = require('kerouac');
+
+var site = kerouac();
+site.engine('pug', require('pug'));
+
+site.set('base url', 'http://www.oauth2orize.org');
+site.set('layout engine', 'pug');
+site.locals.pretty = true;
+
+site.content('content');
+
+site.generate([
+    kerouac.contentCrawler(),
+  ],
+  function(err) {
+    console.log('DONE!');
+    if (err) {
+      console.error(err.message);
+      console.error(err.stack);
+      return;
+    }
+  });
