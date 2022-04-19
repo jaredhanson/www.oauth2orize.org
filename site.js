3b5568ddb19942cd9042d2898ccdbf6ed1b2422a
Jared Hanson <jaredhanson@gmail.com>
2022-04-19 07:24:11 -0700

    Add tailwind typography.

--- site.js
+++ site.js
@@ -9,10 +9,12 @@ site.set('base url', 'http://www.oauth2orize.org');
 site.set('layout engine', 'pug');
 site.locals.pretty = true;
 
+site.use('/concepts/oauth2', require('kerouac-book')('OAuth 2.0', 'books/concepts/oauth2', { layout: 'book' }));
 site.content('content');
 
 site.generate([
     kerouac.contentCrawler(),
+    [ '/concepts/oauth2', require('kerouac-book').browser('books/concepts/oauth2') ],
   ],
   function(err) {
     console.log('DONE!');
