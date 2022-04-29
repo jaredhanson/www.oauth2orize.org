4f67f2dc1876159bc5cf18f357a42ba1328ba029
Jared Hanson <jaredhanson@gmail.com>
2022-04-29 07:05:27 -0700

    Add logger to static file server.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
 
 var app = express();
 
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'www')));
 
 module.exports = app;
