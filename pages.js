018378d9da988cbb919d4040daa4875fac65d565
Jared Hanson <jaredhanson@gmail.com>
2022-04-30 16:58:31 -0700

    Add robots.txt and (temporarily) disabled sitemap.

--- pages.js
+++ pages.js
@@ -11,10 +11,15 @@ site.locals.pretty = true;
 
 site.use('/concepts/oauth2', require('kerouac-book')('OAuth 2.0', 'books/concepts/oauth2', { layout: 'book' }));
 site.content('content');
+//site.page('/sitemap.xml', require('kerouac-sitemap')());
+//site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
+site.page('/robots.txt', require('kerouac-robotstxt')());
 
 site.generate([
     kerouac.contentCrawler(),
     [ '/concepts/oauth2', require('kerouac-book').browser('books/concepts/oauth2', false) ],
+    //[ require('kerouac-sitemap').browser({ index: true }), false ],
+    [ require('kerouac-robotstxt').browser(), false ]
   ],
   function(err) {
     console.log('DONE!');
