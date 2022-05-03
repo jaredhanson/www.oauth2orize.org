b42a49d1b0c52afcaa4c0e6b2d0141e523491de8
Jared Hanson <jaredhanson@gmail.com>
2022-05-03 11:51:54 -0700

    Move book layout.

--- pages.js
+++ pages.js
@@ -9,7 +9,7 @@ site.set('base url', 'http://www.oauth2orize.org');
 site.set('layout engine', 'pug');
 site.locals.pretty = true;
 
-site.use('/concepts/oauth2', require('kerouac-book')('books/concepts/oauth2', { layout: 'book/chapter' }));
+site.use('/concepts/oauth2', require('kerouac-book')('books/concepts/oauth2'));
 site.content('content');
 //site.page('/sitemap.xml', require('kerouac-sitemap')());
 //site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
