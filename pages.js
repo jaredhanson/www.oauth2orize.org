49b578b396725eed41a1a4d1fed600cd95af98f3
Jared Hanson <jaredhanson@gmail.com>
2022-05-20 18:44:27 -0700

    Clean up.

--- pages.js
+++ pages.js
@@ -1,8 +1,9 @@
 var kerouac = require('kerouac');
+var book = require('kerouac-book');
 
 module.exports = [
   kerouac.contentCrawler(),
-  [ '/concepts/oauth2', require('kerouac-book').browser('books/concepts/oauth2', false) ],
+  [ '/concepts/oauth2', book.createMapper('books/concepts/oauth2', false) ],
   //[ require('kerouac-sitemap').browser({ index: true }), false ],
   [ require('kerouac-robotstxt').browser(), false ]
 ];
--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 //require('bixby')('org.kerouacjs/main', { dev: true });
 
 var kerouac = require('kerouac');
+var book = require('kerouac-book');
 
 var site = kerouac();
 site.engine('pug', require('pug'));
@@ -9,7 +10,7 @@ site.set('base url', 'http://www.oauth2orize.org');
 site.set('layout engine', 'pug');
 site.locals.pretty = true;
 
-site.use('/concepts/oauth2', require('kerouac-book')('books/concepts/oauth2'));
+site.use('/concepts/oauth2', book('books/concepts/oauth2'));
 site.use(kerouac.content('content'));
 
 //site.page('/sitemap.xml', require('kerouac-sitemap')());
