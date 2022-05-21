9efc15f6eafe49103e1623211fb46eeb31493f96
Jared Hanson <jaredhanson@gmail.com>
2022-05-21 07:29:50 -0700

    Use createMapper.

--- pages.js
+++ pages.js
@@ -3,7 +3,7 @@ var book = require('kerouac-book');
 var robots = require('kerouac-robotstxt');
 
 module.exports = [
-  kerouac.contentCrawler(),
+  kerouac.content.createMapper(),
   [ '/concepts/oauth2', book.createMapper('books/concepts/oauth2', false) ],
   //[ require('kerouac-sitemap').browser({ index: true }), false ],
   [ robots.createMapper(), false ]
