fe0669702d2530686fbe4ff285c13a0d021a3771
Jared Hanson <jaredhanson@gmail.com>
2022-05-22 15:42:35 -0700

    Generate sitemap.

--- pages.js
+++ pages.js
@@ -1,10 +1,11 @@
 var kerouac = require('kerouac');
 var book = require('kerouac-book');
+var sitemap = require('kerouac-sitemap');
 var robots = require('kerouac-robotstxt');
 
 module.exports = [
   kerouac.content.createMapper(),
   [ '/concepts/oauth2', book.createMapper('books/concepts/oauth2', false) ],
-  //[ require('kerouac-sitemap').browser({ index: true }), false ],
+  [ sitemap.createMapper({ index: true }), false ],
   [ robots.createMapper(), false ]
 ];
--- site.js
+++ site.js
@@ -2,20 +2,21 @@
 
 var kerouac = require('kerouac');
 var book = require('kerouac-book');
+var sitemap = require('kerouac-sitemap');
 var robots = require('kerouac-robotstxt');
 
 var site = kerouac();
 site.engine('pug', require('pug'));
 
-site.set('base url', 'http://www.oauth2orize.org');
+site.set('base url', 'https://www.oauth2orize.org');
 site.set('layout engine', 'pug');
 site.locals.pretty = true;
 
 site.use('/concepts/oauth2', book('books/concepts/oauth2'));
 site.use(kerouac.content('content'));
 
-//site.page('/sitemap.xml', require('kerouac-sitemap')());
-//site.page('/sitemap-index.xml', require('kerouac-sitemap').index());
+site.page('/sitemap.xml', sitemap());
+site.page('/sitemap-index.xml', sitemap.index());
 site.page('/robots.txt', robots());
 
 module.exports = site;
