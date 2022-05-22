97668bb1f48fb66e580c581008dfe9bb8bfa6d94
Jared Hanson <jaredhanson@gmail.com>
2022-05-22 16:56:38 -0700

    Clarify pages.

--- pages.js
+++ pages.js
@@ -3,9 +3,11 @@ var book = require('kerouac-book');
 var sitemap = require('kerouac-sitemap');
 var robots = require('kerouac-robotstxt');
 
-module.exports = [
-  kerouac.content.createMapper(),
-  [ '/concepts/oauth2', book.createMapper('books/concepts/oauth2', false) ],
-  [ sitemap.createMapper({ index: true }), false ],
-  [ robots.createMapper(), false ]
-];
+module.exports = {
+  '/': [
+    kerouac.content.createMapper(),
+    sitemap.createMapper({ index: true }),
+    robots.createMapper()
+  ],
+  '/concepts/oauth2': book.createMapper('books/concepts/oauth2', false)
+};
